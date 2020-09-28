a1558e6668debe3f12c5aee569706526984e5d69
sakkaras <sakkaras.developer@gmail.com>
2020-09-28 13:05:45 +0300

    🎉 Create The Project

---
+++ zoom-scheduler/zoom-scheduler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  zoom-scheduler
+//
+//  Created by Karasuluoglu on 28.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Zoom Scheduler")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ zoom-scheduler/zoom-scheduler/zoom_schedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  zoom_schedulerApp.swift
+//  zoom-scheduler
+//
+//  Created by Karasuluoglu on 28.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct zoom_schedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
